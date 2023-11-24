2be670ece8bee7293801486f12deee7ad4395d53
vectorNull <jose.rodriguez76@protonmail.com>
2023-11-24 15:07:03 -0800

    Create navbar and sidebar

---
+++ app/(platform)/(dashboard)/organization/[organizationId]/layout.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { OrgControl } from './_components/org-control';
+
+const OrganizationIdLayout = ({ 
+  children
+} : {
+  children: React.ReactNode
+}) => {
+  return (
+    <>
+      <OrgControl />
+      {children}
+    </>
+  );
+};
+
+export default OrganizationIdLayout;
